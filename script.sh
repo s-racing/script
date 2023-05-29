43bfefe98142e886724358a0e89517d79508b5b0
satish <satish8840123@gmail.com>
2023-05-29 10:36:19 +0530

    f

--- script.sh
+++ script.sh
@@ -1,36 +1,8 @@
 #!/bin/bash
-
-# Define the name of the feature branch you want to merge
-feature_branch="a"
-
-# Change directory to the desired location
+feature_branch="d"
 cd /var/www/html
-
-# Remove the existing index.html file
 sudo rm -f index.html
-
-# Change directory to the Jenkins workspace
 cd /var/lib/jenkins/workspace/new
-
-# Copy the files to the /var/www/html directory
+echo "Please manually merge the feature branch '$feature_branch' into the main branch"
 sudo cp -R * /var/www/html
 
-# Perform the manual merge
-echo "Please manually merge the feature branch '$feature_branch' into the main branch."
-
-
-
-# Change directory to the Jenkins workspace
-cd /var/lib/jenkins/workspace/new
-
-# Copy the files to the /var/www/html directory
-sudo cp -R * /var/www/html
-
-
-# After merge, reflect the changes in the copied files
-cd /var/www/html
-
-# Add any additional commands to reflect the changes
-# ...
-
-echo "Post-merge script executed successfully!"
